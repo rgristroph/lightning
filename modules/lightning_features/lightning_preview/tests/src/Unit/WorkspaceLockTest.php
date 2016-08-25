1ec40e91fdff473e7d699180b7e95d2bd591baf3
Phéna Proxima <djphenaproxima@gmail.com>
2016-08-25 16:24:21 -0400

    Added more tests of WorkspaceLock>' ' ;

--- modules/lightning_features/lightning_preview/tests/src/Unit/WorkspaceLockTest.php
+++ modules/lightning_features/lightning_preview/tests/src/Unit/WorkspaceLockTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\lightning_preview\Unit;
 
+use Drupal\Core\Config\Entity\ConfigEntityTypeInterface;
+use Drupal\Core\Entity\ContentEntityTypeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\lightning_preview\WorkspaceLock;
 use Drupal\multiversion\Entity\WorkspaceInterface;
@@ -11,7 +13,7 @@ use Drupal\Tests\UnitTestCase;
 use Prophecy\Argument;
 
 /**
- * @coversDefaultClass \Drupal\lightning_preview\WorkspaceLockTest
+ * @coversDefaultClass \Drupal\lightning_preview\WorkspaceLock
  * @group lightning_preview
  */
 class WorkspaceLockTest extends UnitTestCase {
@@ -31,11 +33,11 @@ class WorkspaceLockTest extends UnitTestCase {
   protected $entityTypeManager;
 
   /**
-   * The mocked workspace.
+   * The mocked active workspace.
    *
    * @var WorkspaceInterface|\Prophecy\Prophecy\ObjectProphecy
    */
-  protected $workspace;
+  protected $activeWorkspace;
 
   /**
    * The mocked workspace type.
@@ -58,8 +60,12 @@ class WorkspaceLockTest extends UnitTestCase {
     parent::setUp();
     $this->workspaceManager = $this->prophesize(WorkspaceManagerInterface::class);
     $this->entityTypeManager = $this->prophesize(EntityTypeManagerInterface::class);
-    $this->workspace = $this->prophesize(WorkspaceInterface::class);
+    $this->activeWorkspace = $this->prophesize(WorkspaceInterface::class);
     $this->workspaceType = $this->prophesize(WorkspaceTypeInterface::class);
+
+    $this->workspaceManager->getActiveWorkspace()->willReturn(
+      $this->activeWorkspace->reveal()
+    );
 
     $this->workspaceLock = new WorkspaceLock(
       $this->workspaceManager->reveal(),
@@ -72,14 +78,12 @@ class WorkspaceLockTest extends UnitTestCase {
    *
    * @covers ::isWorkspaceLocked
    */
-  public function testLiveWorkspaceIsNotLocked() {
-    $this->workspace->getMachineName()->willReturn('live');
-    $workspace = $this->workspace->reveal();
-
-    $this->workspaceManager->getActiveWorkspace()->willReturn($workspace);
+  public function testLiveWorkspaceNotLocked() {
+    $this->activeWorkspace->getMachineName()->willReturn('live');
+    $active_workspace = $this->activeWorkspace->reveal();
 
     $this->assertFalse($this->workspaceLock->isWorkspaceLocked());
-    $this->assertFalse($this->workspaceLock->isWorkspaceLocked($workspace));
+    $this->assertFalse($this->workspaceLock->isWorkspaceLocked($active_workspace));
   }
 
   /**
@@ -88,24 +92,59 @@ class WorkspaceLockTest extends UnitTestCase {
    * @covers ::isWorkspaceLocked
    */
   public function testModeratedWorkspaceLock() {
-    $this->workspace->getMachineName()->willReturn('foo');
-    $this->workspace->hasField('moderation_state')->willReturn(TRUE);
-    $workspace = $this->workspace->reveal();
+    $this->activeWorkspace->getMachineName()->willReturn('foo');
+    $this->activeWorkspace->hasField('moderation_state')->willReturn(TRUE);
+    $active_workspace = $this->activeWorkspace->reveal();
 
-    $workspace->type = (object) [
+    $active_workspace->type = (object) [
       'entity' => $this->workspaceType->reveal(),
     ];
     $this->workspaceType
       ->getThirdPartySetting('workbench_moderation', 'locked_states', Argument::any())
       ->willReturn(['archived', 'published']);
 
-    $workspace->moderation_state = (object) [
+    $active_workspace->moderation_state = (object) [
       'target_id' => 'draft',
     ];
-    $this->assertFalse($this->workspaceLock->isWorkspaceLocked($workspace));
+    $this->assertFalse($this->workspaceLock->isWorkspaceLocked($active_workspace));
 
-    $workspace->moderation_state->target_id = 'published';
-    $this->assertTrue($this->workspaceLock->isWorkspaceLocked($workspace));
+    $active_workspace->moderation_state->target_id = 'published';
+    $this->assertTrue($this->workspaceLock->isWorkspaceLocked($active_workspace));
+  }
+
+  /**
+   * Tests workspace locking for config entity types.
+   *
+   * @covers ::isEntityTypeLocked
+   * @depends testLiveWorkspaceNotLocked
+   * @depends testModeratedWorkspaceLock
+   */
+  public function testConfigEntityTypeLock() {
+    $this->assertFalse($this->workspaceLock->isEntityTypeLocked('workspace'));
+
+    $definition = $this->prophesize(ConfigEntityTypeInterface::class)->reveal();
+    $this->entityTypeManager->getDefinition('foo')->willReturn($definition);
+
+    $this->activeWorkspace->getMachineName()->willReturn('live');
+    $this->assertFalse($this->workspaceLock->isEntityTypeLocked('foo'));
+
+    $this->activeWorkspace->getMachineName()->willReturn('stage');
+    $this->assertTrue($this->workspaceLock->isEntityTypeLocked('foo'));
+  }
+
+  /**
+   * Tests workspace locking for content entity types.
+   *
+   * @covers ::isEntityTypeLocked
+   * @depends testLiveWorkspaceNotLocked
+   * @depends testModeratedWorkspaceLock
+   */
+  public function testContentEntityTypeLock() {
+    $definition = $this->prophesize(ContentEntityTypeInterface::class)->reveal();
+    $this->entityTypeManager->getDefinition('foo')->willReturn($definition);
+
+    $this->activeWorkspace->getMachineName()->willReturn('live');
+    $this->assertFalse($this->workspaceLock->isEntityTypeLocked('foo'));
   }
 
 }
